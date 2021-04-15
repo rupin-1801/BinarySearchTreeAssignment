0c6788fb2cf2627a270403bf31d9b647a008988d
rupin-1801 <rupinbhugra1411@gmail.com>
2021-04-15 18:29:28 +0530

    Implemented Node.java class.

--- src/implementation/Node.java
+++ src/implementation/Node.java
@@ -1,5 +1,39 @@
 package implementation;
 
 public class Node<E extends Comparable<E>> implements myinterface.Node<E> {
-    //complete node class
+
+    // variables of Node class..
+    private E data;
+    private Node<E> left;
+    private Node<E> right;
+
+    // Constructor for data..
+    public Node(E data) {
+        this.data = data;
+    }
+
+    // getters and setters..
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public Node<E> getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node<E> left) {
+        this.left = left;
+    }
+
+    public Node<E> getRight() {
+        return right;
+    }
+
+    public void setRight(Node<E> right) {
+        this.right = right;
+    }
 }
